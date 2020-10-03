478dcabfc01148ba598b259ff9ae4c7349b4090e
Anuran Datta <anurdatt@in.ibm.com>
2020-10-04 01:38:40 +0530

    remove webcam-util.ts

--- src/shared/webcam-util.ts
+++ src/shared/webcam-util.ts
@@ -1,41 +1,41 @@
-import * as cv from "opencv4nodejs";
+// import * as cv from "opencv4nodejs";
 
-var wCap:any //new cv.VideoCapture(0)
-var process_id:any
+// var wCap:any //new cv.VideoCapture(0)
+// var process_id:any
 
-//wCap.set(cv.CAP_PROP_FRAME_WIDTH, 320)
-//wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+// //wCap.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+// //wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
 
-function initialize() {
-    if (wCap == undefined) {
-        wCap = new cv.VideoCapture(0)
+// function initialize() {
+//     if (wCap == undefined) {
+//         wCap = new cv.VideoCapture(0)
       
-        wCap.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+//         wCap.set(cv.CAP_PROP_FRAME_WIDTH, 320)
       
-        wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
-    }
+//         wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+//     }
       
-      // if (Webcam = null) {
-      //   Webcam = NodeWebcam.create( opts );
-      // }   
+//       // if (Webcam = null) {
+//       //   Webcam = NodeWebcam.create( opts );
+//       // }   
   
-}
+// }
 
-function reset() {
-    // Webcam.clear()
-    // Webcam = null
-    wCap.release()
-    wCap = undefined
-}
+// function reset() {
+//     // Webcam.clear()
+//     // Webcam = null
+//     wCap.release()
+//     wCap = undefined
+// }
 
-function readFrame():{data:string, rows:number, cols:number} {
-    const frame = wCap.read()
+// function readFrame():{data:string, rows:number, cols:number} {
+//     const frame = wCap.read()
 
-    const data = cv.imencode('.jpg', frame) /*(outCanvas as any)
-        .toBuffer('image/jpeg')*/
-        .toString("base64")
+//     const data = cv.imencode('.jpg', frame) /*(outCanvas as any)
+//         .toBuffer('image/jpeg')*/
+//         .toString("base64")
     
-    return { data, cols:frame.cols, rows: frame.rows }
-}
+//     return { data, cols:frame.cols, rows: frame.rows }
+// }
 
-export { initialize, readFrame, reset }
+// export { initialize, readFrame, reset }
